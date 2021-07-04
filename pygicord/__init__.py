eb13dc0132c6118f88f1b675d2902bb1c9a4f5c8
davidetacchini <davidetacchini11@gmail.com>
2021-07-05 00:52:51 +0200

    version bump to 1.0.0-a; new imports

--- pygicord/__init__.py
+++ pygicord/__init__.py
@@ -1,5 +1,8 @@
+from .base import *
+from .utils import *
+from .button import *
 from .paginator import *
 from .exceptions import *
 
-__version__ = "0.6.0"
+__version__ = "1.0.0-a"
 __author__ = "Davide Tacchini"
