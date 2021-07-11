e648cd79f20dddff44df21419db54a9d0dedaadb
davidetacchini <davidetacchini11@gmail.com>
2021-07-11 15:33:28 +0200

    use enum over intflag

--- pygicord/enums.py
+++ pygicord/enums.py
@@ -1,16 +1,16 @@
-from enum import IntFlag
+from enum import Enum
 
 __all__ = ("StopAction", "Config")
 
 
-class StopAction(IntFlag):
-    DO_NOTHING = 0x0
-    DELETE_MESSAGE = 0x1
-    CLEAR_REACTIONS = 0x2
+class StopAction(Enum):
+    DO_NOTHING = 0
+    DELETE_MESSAGE = 1
+    CLEAR_REACTIONS = 2
 
 
-class Config(IntFlag):
-    DEFAULT = 0x1  # controller: first, previous, stop, next, last, input
-    MINIMAL = 0x2  # controller: previous, stop, next
-    PLAIN = 0x4  # controller: first, previous, stop, next
-    RICH = 0x8  # controller: all defaults + lock
+class Config(Enum):
+    DEFAULT = 1  # controller: first, previous, stop, next, last, input
+    MINIMAL = 2  # controller: previous, stop, next
+    PLAIN = 3  # controller: first, previous, stop, next
+    RICH = 4  # controller: all defaults + lock
